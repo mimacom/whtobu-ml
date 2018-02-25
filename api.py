c272d9444d2572367008b10b1c1abdbc3cb82e39
deacix <info@deacix.de>
2018-02-25 22:14:36 +0100

    added analyze logic | sk

--- api.py
+++ api.py
@@ -1,16 +1,71 @@
 #!flask/bin/python
-from flask import Flask, jsonify, make_response, request, abort, flash
-import uuid
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
 import os
+import tensorflow as tf
+import time
+import uuid
+from flask import Flask, jsonify, make_response, request, abort
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '/tmp'
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+def load_graph(model_file):
+    graph = tf.Graph()
+    graph_def = tf.GraphDef()
+
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+
+    return graph
+
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+                                input_mean=0, input_std=255):
+    input_name = "file_reader"
+    output_name = "normalized"
+    file_reader = tf.read_file(file_name, input_name)
+    if file_name.endswith(".png"):
+        image_reader = tf.image.decode_png(file_reader, channels=3,
+                                           name='png_reader')
+    elif file_name.endswith(".gif"):
+        image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                      name='gif_reader'))
+    elif file_name.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(file_reader, channels=3,
+                                            name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0);
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
+
+    return result
+
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
 
 
 @app.route('/analyze', methods=['POST'])
@@ -33,6 +88,49 @@ def analyze():
     print(app.config['UPLOAD_FOLDER'] + "/" + filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+    file_name = app.config['UPLOAD_FOLDER'] + "/" + filename
+    model_file = "graph.pb"
+    label_file = "labels.txt"
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    input_layer = "input"
+    output_layer = "final_result"
+
+    graph = load_graph(model_file)
+    t = read_tensor_from_image_file(
+        file_name,
+        input_height=input_height,
+        input_width=input_width,
+        input_mean=input_mean,
+        input_std=input_std
+    )
+
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
+
+    with tf.Session(graph=graph) as sess:
+        start = time.time()
+        results = sess.run(
+            output_operation.outputs[0],
+            {input_operation.outputs[0]: t}
+        )
+
+        end = time.time()
+
+    results = np.squeeze(results)
+
+    top_k = results.argsort()[-5:][::-1]
+    labels = load_labels(label_file)
+
+    print('\nEvaluation time (1-image): {:.3f}s\n'.format(end - start))
+
+    for i in top_k:
+        print(labels[i], results[i])
+
     return jsonify([
         {
             'ASIN': 'ABCDF'
@@ -46,7 +144,7 @@ if __name__ == '__main__':
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-   # sess.init_app(app)
+    # sess.init_app(app)
 
     app.debug = True
     app.run()
