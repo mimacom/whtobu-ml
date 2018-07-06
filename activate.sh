5afb9cf166d47bbaf21d0d18535af7042e44a96d
Sergej Kunz <sergej.kunz@mimacom.com>
2018-07-06 11:59:49 +0200

    added cuda 9 to ld path

--- activate.sh
+++ activate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ml/bin/activate
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64/:/usr/local/cuda-9.0/lib64/"
