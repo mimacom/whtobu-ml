2599f6da053f5a3e7585177b0ab7fdecc21507c5
Sergej Kunz <sergej.kunz@mimacom.com>
2018-07-08 14:59:49 +0200

    optimization

--- train.py
+++ train.py
@@ -9,10 +9,13 @@ data =  tc.SFrame('images.sframe')
 train_data, test_data = data.random_split(0.8)
 
 # Create a model
-model = tc.object_detector.create(train_data, max_iterations=20000)
+model = tc.object_detector.create(train_data, feature='image', annotations='annotations')
 
 # Save predictions to an SArray
 predictions = model.predict(test_data)
+
+predictions_stacked = tc.object_detector.util.stack_annotations(predictions)
+print(predictions_stacked)
 
 # Evaluate the model and save the results into a dictionary
 metrics = model.evaluate(test_data)
